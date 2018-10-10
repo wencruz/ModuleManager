d30d4452e7022f0486a5b40a9e1ccb6bc95fa8f5
Lisias T <me@lisias.net>
2018-10-10 15:59:45 -0300

    Some weird situation preventing KSP to "upload" to the GameDatabase the new TechTree we detected and, hopefully, workarounded.

--- Source/ModuleManager/CustomConfigsManager.cs
+++ Source/ModuleManager/CustomConfigsManager.cs
@@ -7,20 +7,30 @@ namespace ModuleManager
     [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
     public class CustomConfigsManager : MonoBehaviour
     {
+		static bool start_techtree_loaded = false;
+		static bool start_physics_loaded = false;
         internal void Start()
         {
-			if (HighLogic.CurrentGame.Parameters.Career.TechTreeUrl != MMPatchLoader.TECHTREE_CONFIG.Path && MMPatchLoader.TECHTREE_CONFIG.IsLoadable)
+#if false
+			Log("Blah");
+            Log(HighLogic.CurrentGame.Parameters.Career.TechTreeUrl);
+			Log(MMPatchLoader.TECHTREE_CONFIG.Path);
+			Log(MMPatchLoader.TECHTREE_CONFIG.IsLoadable.ToString());
+#endif            
+			if (!start_techtree_loaded && MMPatchLoader.TECHTREE_CONFIG.IsLoadable)
             {
                 Log("Setting modded tech tree as the active one");
-                HighLogic.CurrentGame.Parameters.Career.TechTreeUrl = MMPatchLoader.TECHTREE_CONFIG.Path;
+                HighLogic.CurrentGame.Parameters.Career.TechTreeUrl = MMPatchLoader.TECHTREE_CONFIG.KspPath;
+				start_techtree_loaded = true;
             }
 
-			if (PhysicsGlobals.PhysicsDatabaseFilename != MMPatchLoader.PHYSICS_CONFIG.Path && MMPatchLoader.PHYSICS_CONFIG.IsLoadable)
+			if (!start_physics_loaded && MMPatchLoader.PHYSICS_CONFIG.IsLoadable)
             {
                 Log("Setting modded physics as the active one");
                 PhysicsGlobals.PhysicsDatabaseFilename = MMPatchLoader.PHYSICS_CONFIG.Path;
                 if (!PhysicsGlobals.Instance.LoadDatabase())
                     Log("Something went wrong while setting the active physics config.");
+                start_physics_loaded = true;
             }
         }
 
