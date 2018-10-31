f8ebda33b32900251098dc9db737704de18f29d0
blowfish <joeydwong@gmail.com>
2018-10-30 22:48:55 -0700

    Extract paths to their own class
    Forgot that the static initalizers would prevent tests from running

---
+++ ModuleManager/CustomConfigsManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+using static ModuleManager.FilePathRepository;
+
+namespace ModuleManager
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    public class CustomConfigsManager : MonoBehaviour
+    {
+        internal void Start()
+        {
+            if (HighLogic.CurrentGame.Parameters.Career.TechTreeUrl != techTreeFile &&  File.Exists(techTreePath))
+            {
+                Log("Setting modded tech tree as the active one");
+                HighLogic.CurrentGame.Parameters.Career.TechTreeUrl = techTreeFile;
+            }
+
+            if (PhysicsGlobals.PhysicsDatabaseFilename != physicsFile && File.Exists(physicsPath))
+            {
+                Log("Setting modded physics as the active one");
+
+                PhysicsGlobals.PhysicsDatabaseFilename = physicsFile;
+
+                if (!PhysicsGlobals.Instance.LoadDatabase())
+                    Log("Something went wrong while setting the active physics config.");
+            }
+        }
+
+        public static void Log(String s)
+        {
+            print("[CustomConfigsManager] " + s);
+        }
+
+    }
+}
