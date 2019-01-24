9673ce55e440a653201f18a0ee15f0121b0b463c
Lisias T <me@lisias.net>
2019-01-24 21:50:34 -0200

    FIxing the mess that merging upstream into a heavily refactored code causes. :P

--- Source/ModuleManager/CustomConfigsManager.cs
+++ Source/ModuleManager/CustomConfigsManager.cs
@@ -2,42 +2,35 @@
 using System.IO;
 using UnityEngine;
 
+using static ModuleManager.FilePathRepository;
+
 namespace ModuleManager
 {
     [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
     public class CustomConfigsManager : MonoBehaviour
     {
-		static bool start_techtree_loaded = false;
-		static bool start_physics_loaded = false;
         internal void Start()
         {
-#if false
-			Log("Blah");
-            Log(HighLogic.CurrentGame.Parameters.Career.TechTreeUrl);
-			Log(MMPatchLoader.TECHTREE_CONFIG.Path);
-			Log(MMPatchLoader.TECHTREE_CONFIG.IsLoadable.ToString());
-#endif            
-			if (!start_techtree_loaded && MMPatchLoader.TECHTREE_CONFIG.IsLoadable)
+            if (HighLogic.CurrentGame.Parameters.Career.TechTreeUrl != techTreeFile &&  File.Exists(techTreePath))
             {
                 Log("Setting modded tech tree as the active one");
-                HighLogic.CurrentGame.Parameters.Career.TechTreeUrl = MMPatchLoader.TECHTREE_CONFIG.KspPath;
-				start_techtree_loaded = true;
+                HighLogic.CurrentGame.Parameters.Career.TechTreeUrl = techTreeFile;
             }
 
-			if (!start_physics_loaded && MMPatchLoader.PHYSICS_CONFIG.IsLoadable)
+            if (PhysicsGlobals.PhysicsDatabaseFilename != physicsFile && File.Exists(physicsPath))
             {
                 Log("Setting modded physics as the active one");
-                PhysicsGlobals.PhysicsDatabaseFilename = MMPatchLoader.PHYSICS_CONFIG.Path;
+
+                PhysicsGlobals.PhysicsDatabaseFilename = physicsFile;
+
                 if (!PhysicsGlobals.Instance.LoadDatabase())
                     Log("Something went wrong while setting the active physics config.");
-                start_physics_loaded = true;
             }
         }
 
-		private static readonly KSPe.Util.Log.Logger log = KSPe.Util.Log.Logger.CreateForType<CustomConfigsManager>();
-        private static void Log(String s)
+        public static void Log(String s)
         {
-            log.info(s);
+            print("[CustomConfigsManager] " + s);
         }
 
     }
