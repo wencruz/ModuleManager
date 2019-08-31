8bbfec17e9749ab735816aa3dd30fa2e26c5b70c
Lisias T <me@lisias.net>
2019-08-31 13:48:59 -0300

    Printing forkname and version on startup. I saw some KSP users using it, and I need to differentiate myself from the mainstream.

--- Source/ModuleManager/ModuleManager.cs
+++ Source/ModuleManager/ModuleManager.cs
@@ -13,6 +13,15 @@ using ModuleManager.Logging;
 
 namespace ModuleManager
 {
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    internal class Startup : MonoBehaviour
+	{
+        private void Start()
+        {
+            ModLogger.LOG.force("Version {0}", Version.Text);
+        }
+	}
+
     [KSPAddon(KSPAddon.Startup.Instantly, false)]
     public class ModuleManager : MonoBehaviour
     {
