369b6b73ba3e1b03cbbbb10542437f15dce9ed51
Lisias T <me@lisias.net>
2019-10-18 18:13:30 -0300

    Moving pre-initialization code to it's own class and file. Saner.

--- Source/ModuleManager/ModuleManager.cs
+++ Source/ModuleManager/ModuleManager.cs
@@ -13,15 +13,6 @@ using ModuleManager.Logging;
 
 namespace ModuleManager
 {
-    [KSPAddon(KSPAddon.Startup.Instantly, true)]
-    internal class Startup : MonoBehaviour
-	{
-        private void Start()
-        {
-            ModLogger.LOG.force("Version {0}", Version.Text);
-        }
-	}
-
     [KSPAddon(KSPAddon.Startup.Instantly, false)]
     public class ModuleManager : MonoBehaviour
     {
