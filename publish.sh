9dcc1f12c35630615c94ecb170ded12ab177f8cf
Lisias <me@lisias.net>
2020-06-16 09:17:34 -0300

    Changing publishing target.

--- publish.sh
+++ publish.sh
@@ -5,5 +5,5 @@ source ./CONFIG.inc
 VERSIONFILE=$PACKAGE.version
 
 scp -i $SSH_ID "./GameData/$TARGETDIR/$VERSIONFILE" $SITE:/$TARGET_CONTENT_PATH
-scp -i $SSH_ID "./GameData/$TARGETDIR/$PACKAGE.README.md" $SITE:/${TARGET_CMS_PATH}${PACKAGE}.md
+scp -i $SSH_ID "./GameData/$TARGETDIR/$PACKAGE.README.md" $SITE:/${TARGET_CMS_PATH}${PACKAGE}/index.md
 #scp -i $SSH_ID "./PR_material/banner.jpg" $SITE:/${TARGET_CONTENT_PATH}PR_material/banner.jpg
